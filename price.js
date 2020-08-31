871566e2fd0257a7abd4f1c1a2bda6dd64949804
unknown <godstimeonyibe2@gmail.com>
2020-08-31 02:35:30 +0100

    i added local storage

--- price.js
+++ price.js
@@ -5,18 +5,16 @@ const nightmare = nightMare({ show: true })
 async function checkPrice () {
   const priceStr = await nightmare.goto('https://www.amazon.com/Roam-Universal-Premium-Phone-Motorcycle/dp/B01LWDCSIZ')
     .wait('#priceblock_saleprice')
-    .evaluate(() => document.getElementById("priceblock_saleprice").innerText)
-    // .on('page', function(type="alert", message))
+    .evaluate(() => window.localStorage.setItem("currentPrice","priceblock_saleprice"))
     .end()
   const priceNum = parseFloat(priceStr.replace('$', ''))
-  
-  // const targetPrice = document.getElementById('targetPrice').innerText = 20
+
   let targetPrice = 20
   console.log(priceNum)
-  // if (priceNum <= targetPrice) {
-  //   console.log('It is cheap')
-  // } else {
-  //   console.log('It is expensive')
-  // }
+  if (priceNum <= targetPrice) {
+    console.log('It is cheap')
+  } else {
+    console.log('It is expensive')
+  }
 }
 checkPrice()
